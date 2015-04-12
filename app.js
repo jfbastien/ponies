9b1134521d00c81ff5d6df839e9d78143782115f
JF Bastien <jfb@google.com>
2015-04-12 18:28:02 +0100

    Add Security, hack, and JavaScript slides.

--- app.js
+++ app.js
@@ -10,16 +10,19 @@ window.addEventListener("beforeunload", function (e) {
 var template = document.querySelector('#t');
 
 template.pages = [
-  {name: 'C++ on the Web',  hash: 'intro',   icon: 'info-outline'},
-  {name: 'PNaCl',           hash: 'pnacl',   icon: 'label-outline'},
-  {name: 'Coming Soon',     hash: 'coming',  icon: 'label-outline'},
-  {name: 'Life',            hash: 'life',    icon: 'picture-in-picture'},
-  {name: 'Flock',           hash: 'flock',   icon: 'picture-in-picture'},
-  {name: 'Voronoi',         hash: 'voronoi', icon: 'picture-in-picture'},
-  {name: 'DevEnv',          hash: 'devenv',  icon: 'cloud-queue'},
-  {name: 'Randomization',   hash: 'random',  icon: 'label'},
-  {name: 'Fuzzing',         hash: 'fuzzing', icon: 'label'},
-  {name: 'The End',         hash: 'outro',   icon: 'info'},
+  {name: 'C++ on the Web',  hash: 'intro',    icon: 'info-outline'},
+  {name: 'PNaCl',           hash: 'pnacl',    icon: 'label-outline'},
+  {name: 'Coming Soon',     hash: 'coming',   icon: 'label-outline'},
+  {name: 'Life',            hash: 'life',     icon: 'picture-in-picture'},
+  {name: 'Flock',           hash: 'flock',    icon: 'picture-in-picture'},
+  {name: 'Voronoi',         hash: 'voronoi',  icon: 'picture-in-picture'},
+  {name: 'DevEnv',          hash: 'devenv',   icon: 'cloud-queue'},
+  {name: 'Security',        hash: 'security', icon: 'label'},
+  {name: 'Randomization',   hash: 'random',   icon: 'label'},
+  {name: 'Fuzzing',         hash: 'fuzzing',  icon: 'label'},
+  {name: 'Hack Me',         hash: 'hack',     icon: 'label'},
+  {name: 'JavaScript?',     hash: 'js',       icon: 'label'},
+  {name: 'The End',         hash: 'outro',    icon: 'info'},
 ];
 
 template.menuItemSelected = function(e, detail, sender) {
